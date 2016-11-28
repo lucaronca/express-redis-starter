4d48a2e6988917dc070863fc64ce5830b7fee2ad
Luca Ronca <Luca Ronca>
2016-11-28 20:43:45 +0100

    changed info and comments

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const redis = require('redis'),
+	//creates a new client
 	client = redis.createClient();
 
 client.on('connect', function() {
 	console.log('connected to redis');
 });
 
- //creates a new client
 module.exports = client;

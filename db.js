348a27541988814f67da63fe60d6476d0856283b
luca.ronca <Cenerentola2>
2016-11-29 18:53:14 +0100

    edited create method in document model

--- db.js
+++ db.js
@@ -1,9 +1,21 @@
 const redis = require('redis'),
 	//creates a new client
-	client = redis.createClient();
+	client = redis.createClient( 7000, '127.0.0.1', {'return_buffers': true} );
 
 client.on('connect', function() {
 	console.log('connected to redis');
 });
+
+redis.on('error', errorHandler);
+
+client.on('error', errorHandler);
+
+client.on('warning', function(warn) {
+	console.warn(warn);
+});
+
+function errorHandler(err) {
+	console.error('Redis error', err);
+}
 
 module.exports = client;
--- models/document.js
+++ models/document.js
@@ -1,19 +1,26 @@
 const client = require('../db.js');
 
 // Create new document in your database and return its id
-exports.create = function(id, content, date, cb) {
+exports.create = function(id, title, content, date, cb) {
 
-	let document = {
-		id,
-		content,
+	//@TODO check if content is buffer, create id
+
+	let dataInfo = {
+		title,
 		date: {
 			month: date.month,
 			yeat: date.year
 		},
 		actualDate: new Date().ToString()
-	}
+	};
 
-	client.set('document', document);
+	// My object will be an array with a random key, content is the first value, object data the second
+
+	var key = generateKey();
+
+	client.lpush(key, content);
+
+	client.lpush(key, JSON.stringify(dataInfo));
 
 }
 
@@ -24,7 +31,30 @@ exports.get = function(id, cb) {
 
 // Get all comments
 exports.all = function(cb) {
-  cb(null, [])
+
+	client.lrange('documents', 0, -1, function(err, documents){
+
+		errorHandler(err);
+
+		cb(null, documents);
+
+	});
+
+}
+
+function errorHandler(err) {
+
+	if (err) {
+		console.error('error in create model');
+		throw new Error(err);
+	}
+
+}
+
+function generateKey() {
+
+    return ("0000" + (Math.random()*Math.pow(36,4) << 0).toString(36)).slice(-4);
+
 }
 
 // Get all comments by a particular user

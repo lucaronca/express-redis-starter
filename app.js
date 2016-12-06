a42e302163c5a8314ab8143614a2227614bd0796
luca.ronca <Cenerentola2>
2016-12-06 10:20:02 +0100

    Fix static folder name

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, '/static')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));

fad772c1c0562708b5d5a4b04387d3381cf09a32
Luca Ronca <Luca Ronca>
2016-12-08 19:38:12 +0100

    changed post endpoint call

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,63 +1,69 @@
-$('.upload-btn').on('click', function (){
-    $('#upload-input').click();
-    $('.progress-bar').text('0%');
-    $('.progress-bar').width('0%');
-});
+$(document).ready(function(){
 
-$('#upload-input').on('change', function(){
+  $('.upload-btn').on('click', function (){
+      $('#upload-input').click();
+      $('.progress-bar').text('0%');
+      $('.progress-bar').width('0%');
+  });
 
-  var files = $(this).get(0).files;
+  $('#upload-input').on('change', function(){
 
-  if (files.length > 0){
-    // create a FormData object which will be sent as the data payload in the
-    // AJAX request
-    var formData = new FormData();
+    var files = $(this).get(0).files;
 
-    // loop through all the selected files and add them to the formData object
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
+    if (files.length > 0){
+      // create a FormData object which will be sent as the data payload in the
+      // AJAX request
+      var formData = new FormData();
 
-      // add the files to formData object for the data payload
-      formData.append('uploads[]', file, file.name);
-    }
+      // loop through all the selected files and add them to the formData object
+      for (var i = 0; i < files.length; i++) {
+        var file = files[i];
 
-    $.ajax({
-      url: '/upload',
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: function(data){
-          console.log('upload successful!\n' + data);
-      },
-      xhr: function() {
-        // create an XMLHttpRequest
-        var xhr = new XMLHttpRequest();
+        // add the files to formData object for the data payload
+        formData.append('uploads[]', file, file.name);
+      }
 
-        // listen to the 'progress' event
-        xhr.upload.addEventListener('progress', function(evt) {
+      $.ajax({
+        url: '/upload/send',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(data){
+            console.log('upload successful!\n' + data);
+        },
+        xhr: function() {
+          // create an XMLHttpRequest
+          var xhr = new XMLHttpRequest();
 
-          if (evt.lengthComputable) {
-            // calculate the percentage of upload completed
-            var percentComplete = evt.loaded / evt.total;
-            percentComplete = parseInt(percentComplete * 100);
+          // listen to the 'progress' event
+          xhr.upload.addEventListener('progress', function(evt) {
 
-            // update the Bootstrap progress bar with the new percentage
-            $('.progress-bar').text(percentComplete + '%');
-            $('.progress-bar').width(percentComplete + '%');
+            if (evt.lengthComputable) {
+              // calculate the percentage of upload completed
+              var percentComplete = evt.loaded / evt.total;
+              percentComplete = parseInt(percentComplete * 100);
 
-            // once the upload reaches 100%, set the progress bar text to done
-            if (percentComplete === 100) {
-              $('.progress-bar').html('Done');
+              // update the Bootstrap progress bar with the new percentage
+              $('.progress-bar').text(percentComplete + '%');
+              $('.progress-bar').width(percentComplete + '%');
+
+              // once the upload reaches 100%, set the progress bar text to done
+              if (percentComplete === 100) {
+                $('.progress-bar').html('Done');
+              }
+
             }
 
-          }
+          }, false);
 
-        }, false);
+          return xhr;
+        }
+      });
 
-        return xhr;
-      }
-    });
-
-  }
+    }
+  });
+  
 });
+
+

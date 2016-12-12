62aee61f8859833b79ae2947b3fa0864fbf2f05b
luca.ronca <Cenerentola2>
2016-12-12 18:42:56 +0100

    added home controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,8 @@ const
 	express = require('express'),
 	router = express.Router();
 
+router.use('/', require('./home'));
 router.use('/comments', require('./comments'));
 router.use('/upload', require('./upload'));
 
-router.get('/', (req, res) => {
-  res.render('pages/home');
-})
-
-module.exports = router
+module.exports = router;

18e0c3a3e9ba2679a8ec2eb10401cadf71c79b4c
luca.ronca <Cenerentola2>
2016-12-15 17:33:10 +0100

    added logic for rendering of all documents

--- controllers/home.js
+++ controllers/home.js
@@ -9,9 +9,16 @@ router.get('/', (req, res, next) => {
 
 		if (err) return next(err);
 
+		// change documets display order
+		let reverse = (req.query.reverse === 'true') ? true : false ;
+
+		if (reverse) data.reverse();
+
+		res.render('pages/home', { data, reversed: reverse });
+
 	});
 
-	res.render('pages/home');
+	
 })
 
 module.exports = router;

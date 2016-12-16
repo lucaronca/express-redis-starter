e99029d90c6971375b9bc7391c40c08897a03c04
luca.ronca <Cenerentola2>
2016-12-16 18:38:12 +0100

    added static folder for uploads

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.set('port', (process.env.PORT || 5000));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/docs', express.static(path.join(__dirname, '/uploads')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));

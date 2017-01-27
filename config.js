f0906f01c990cc545ca42fa14b51dae72e2e910e
luca.ronca <Cenerentola2>
2017-01-27 18:36:40 +0100

    created a global configuration file and moved to the root

--- config.js
+++ config.js
@@ -2,9 +2,16 @@ const path = require('path');
 
 exports.webpack_paths = {
 
-    src: path.join(__dirname, '..', 'client', 'src'),
-    build: path.join(__dirname, '..', 'public', 'build')
+    src: path.join(__dirname, 'client', 'src'),
+    build: path.join(__dirname, 'client', 'build')
+};
 
+exports.webpack_entries = {
+    vendor: ['jquery', 'foundation-sites'],
+    home: ['index.js', 'styles.scss'],
+    login: ['index.js', 'styles.scss'],
+    upload: ['index.js', 'styles.scss'],
+    common: ['styles.scss']
 };
 
 exports.port = process.env.PORT || 5000;

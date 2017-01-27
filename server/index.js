4c897a1e10ae233117a96b949ab6f43faad69e7f
luca.ronca <Cenerentola2>
2017-01-27 18:45:32 +0100

    moved some server instruction from app.js to here and edited comments

--- server/index.js
+++ server/index.js
@@ -2,34 +2,39 @@ const
     path = require('path'),
     express = require('express'),
     app = express.Router(),
+    cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     session = require('express-session'),
     client = require('./data/db'),
     passport = require('passport'),
-    RedisStore = require('connect-redis')(session);
+    RedisStore = require('connect-redis')(session),
+    open = require('open'),
+    config = require('../config');
 
-// Error middleware
+//
+// Register middlewares
+// ---------------------------------------------------------------------------------------
 app.use(require('./middlewares/error'));
-
-// View Engine
-app.set('views', path.join(__dirname, 'server', 'views'));
-app.set('view engine', 'ejs');
-
-// public paths
-app.use('/build', express.static(path.join(__dirname, 'public', 'build')));
-app.use('/docs', express.static(path.join(__dirname, 'public', 'uploads')));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-// Adding a common render
 app.use(require('./middlewares/commonRender'));
 
+//
+// static paths
+// ---------------------------------------------------------------------------------------
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/build', express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use('/docs', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//
 // Authentication
+// ---------------------------------------------------------------------------------------
 const Auth = require('./helpers/Auth');
 new Auth().setup();
 
-// create a new session and store it existing redis client
+// Create a new session and store it in an existing redis client
 app.use(session({
     store: new RedisStore({
         client: client
@@ -39,12 +44,24 @@ app.use(session({
     saveUninitialized: false
 }));
 
-// init passport authentication
+// Init passport authentication
 app.use(passport.initialize());
 // persistent login sessions
 app.use(passport.session());
 
-// App logic
+//
+// Bootsrap server logic
+// ---------------------------------------------------------------------------------------
 app.use(require('./controllers'));
 
-module.exports = app;
+//
+// Function for start the server
+// ---------------------------------------------------------------------------------------
+exports.start = (app) => {
+    if (app.get('env') === 'development') open('http://localhost:' + config.port);
+    app.listen(config.port, () => {
+        console.log(`Listening on port ${config.port}...`)
+    });
+};
+
+exports.app = app;

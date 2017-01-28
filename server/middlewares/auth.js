94d756069efd01fcda21c6fca25cdf22bbef7954
Luca Ronca <luca.ronca07@gmail.com>
2017-01-28 14:57:02 +0100

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,8 +1,6 @@
-module.exports = function() {
-    return function (req, res, next) {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        res.redirect('/login');
+module.exports = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
     }
-};
+    res.redirect('/login');
+}

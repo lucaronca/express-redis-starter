c9c424ae62e1f4c5075cfef31dbe6a33c8ec8a95
Luca Ronca <luca.ronca07@gmail.com>
2017-01-28 14:55:34 +0100

    Update upload.js

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -1,7 +1,7 @@
 const
 	express = require('express'),
 	router = express.Router(),
-    passport = require('passport'),
+    	passport = require('passport'),
 	path = require('path'),
 	formidable = require('formidable'),
 	fs = require('fs'),
